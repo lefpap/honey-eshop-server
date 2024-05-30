b8eafad25b87c69c713f1ee5ec34dac2d4518b8d
lefpap <lolopap21@gmail.com>
2024-05-30 17:48:22 +0300

    chore: Remove unused imports and add logging in ProductController

--- src/main/java/com/lefpap/honeyeshop/product/ProductController.java
+++ src/main/java/com/lefpap/honeyeshop/product/ProductController.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,14 +21,18 @@ public class ProductController {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Product> queryProducts(@Valid @ModelAttribute ProductsFilters filters) {
+        LOG.info("Querying products with filters: {}", filters);
         List<Product> products = productDao.findAll(filters.toSpecification());
+        LOG.info("Found {} products", products.size());
         return products;
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Product getProduct(@PathVariable(name = "id") Long productId) {
+        LOG.info("Getting product with id: {}", productId);
         Product product = productDao.findById(productId).orElseThrow(EntityNotFoundException::new);
+        LOG.info("Found product: {}", product);
         return product;
     }
 }
